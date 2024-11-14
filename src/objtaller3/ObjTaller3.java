e57e4bb3d58b71052ede0b690eaf1503f6661af5
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-13 21:06:41 -0500

    Update ObjTaller3.java

--- src/objtaller3/ObjTaller3.java
+++ src/objtaller3/ObjTaller3.java
@@ -33,7 +33,7 @@ public class ObjTaller3 {
         System.out.println(emp2.cedula + " va a retirar producto");
         orden2.retirarProducto(emp2, p4);
         System.out.println(Producto.getTotalProductosPedidos());
-        orden2.retirarProducto(**, p2);
+        orden2.retirarProducto(emp1, p2);
         System.out.println(Producto.getTotalProductosPedidos());
     }
 }
